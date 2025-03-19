6fd9bc4e2e2ca6f46f5b1dac42050f614e10c38e
Lukasz-Sajdak <el.sajdak320@gmail.com>
2025-03-19 09:46:20 +0100

    I made polymorphism template

---
+++ Homework2_polymorphism/Robot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2_polymorphism
+{
+    class Robot
+    {
+        protected string Type;
+
+        public int AnnualCost()
+        {
+            return 0;
+        }
+
+    }
+}
