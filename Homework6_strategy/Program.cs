f77afd4867b8e19c73a430d176a4335250891011
Lukasz-Sajdak <el.sajdak320@gmail.com>
2025-04-29 15:52:45 +0200

    Homework6 template

---
+++ Homework6_strategy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework6_strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.Run();
+        }
+    }
+}
---
+++ Homework6_strategy/Strategy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework6_strategy
+{
+    interface IStrategy
+    {
+        bool GetNextMove(List<bool> knownMoves); // use this method to return your next move in game
+    }
+}
