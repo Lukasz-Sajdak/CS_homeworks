c9a46da5ad8f961c515d419ec6a9ad32dbf29e5c
Lukasz-Sajdak <el.sajdak320@gmail.com>
2025-04-29 20:15:54 +0200

    strategy implementation

---
+++ Homework6_strategy/StrategyDetective.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6_strategy
+{
+    class StrategyDetective : IStrategy
+    {
+        private int round = 0;
+
+        public bool GetNextMove(List<bool> knownMoves)
+        {
+            round++;
+            if (round == 1) return true;
+            if (round == 2) return false;
+            if (round == 3) return true;
+            if (round == 4) return true;
+
+            return knownMoves[knownMoves.Count - 1];
+        }
+    }
+}
---
+++ Homework6_strategy/StrategyGrudger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6_strategy
+{
+    class StrategyGrudger : IStrategy
+    {
+        private bool hasBeenBetrayed = false;
+
+        public bool GetNextMove(List<bool> knownMoves)
+        {
+            if (hasBeenBetrayed)
+                return false;
+            if (knownMoves.Contains(false))
+                hasBeenBetrayed = true;
+            return true;
+        }
+    }
+}
