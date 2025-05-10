69388f57ad248313fca2f563019f9976d4d97d23
Lukasz-Sajdak <el.sajdak320@gmail.com>
2025-05-10 05:55:12 +0200

    first commit

---
+++ Homework7_commands/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7_commands
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
